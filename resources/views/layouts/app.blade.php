0e6d53b0153a1b610f95f7f90b8983a139df949f
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-08-25 20:37:57 -0500

    [ADD] Verificación de correo electrónico + UI de navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -39,16 +39,21 @@
                         </li>
                     @endif
                 @else
-                    <li>
+                    <li class="flex items-center">
                         <a id="navbarDropdown" class="inline-block py-2 px-4 hover:bg-carbon-600 dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                             {{ Auth::user()->name }} <span class="caret"></span>
                         </a>
 
-                        <div aria-labelledby="navbarDropdown">
-                            <a href="{{ route('logout') }}"
+                        <div aria-labelledby="navbarDropdown" class="ml-2">
+                            <a 
+                                title="Cerrar sesión"
+                                href="{{ route('logout') }}" 
+                                class="rounded-full border border-carbon-500 p-2 flex items-center justify-center bg-carbon-600 text-carbon-200 hover:text-white hover:bg-carbon-500 hover:border-carbon-400"
                                 onclick="event.preventDefault();
-                                                document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
+                                                document.getElementById('logout-form').submit();"
+                            >
+                                <!-- {{ __('Logout') }} -->
+                                <svg viewBox="0 0 20 20" fill="currentColor" class="logout w-4 h-4 inline-block"><path fill-rule="evenodd" d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z" clip-rule="evenodd"></path></svg>
                             </a>
 
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
