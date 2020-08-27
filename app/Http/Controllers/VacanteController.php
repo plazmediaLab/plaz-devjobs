8d9b4898af660cb2ac24706cfcbff8796072dd7e
AdrianGD1337 <contacto.plazmedia@gmail.com>
2020-08-27 16:39:32 -0500

    [ADD] Controlador Vacante creado + -mcr

---
+++ app/Http/Controllers/VacanteController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Vacante;
+use Illuminate\Http\Request;
+
+class VacanteController extends Controller
+{
+
+    // Protejer ruta ante autenticación y verificación del usuario 
+    public function __construct()
+    {
+        // Revisar que el usuario esta autenticado
+        $this->middleware(['auth', 'verified']);
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return "Desde VacantesController@Index";
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Vacante  $vacante
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Vacante $vacante)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Vacante  $vacante
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Vacante $vacante)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Vacante  $vacante
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Vacante $vacante)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Vacante  $vacante
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Vacante $vacante)
+    {
+        //
+    }
+}
